0287bd03f3482aa88362f5df229224a382e62612
Ubuntu <ubuntu@ip-172-31-20-169.us-east-2.compute.internal>
2020-02-03 08:11:49 +0000

    Complete JSX & Props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from 'prop-types'
+
+function Food({ name , number, rating }) {
+    return(
+    <div>
+        <h1>I like { name }</h1>
+        <p> rating is { rating } </p>
+        <h2 alt={ name }>number is { number }</h2>
+    </div>
+    );
+}
+
+Food.propTypes = {
+    name: PropTypes.string.isRequired,
+    rating: PropTypes.number,
+    number: PropTypes.string.isRequired
+};
+
+const foodILike = [
+    {
+        id:1,
+        name:"kimchi",
+        number:'1',
+        rating: 4.2
+    },
+    {
+        id:2,
+        name:"something1",
+        number:'2',
+        rating:4.5
+    },
+    {
+        id:3,
+        name:"something2",
+        number:'3',
+        rating:5.0
+    },
+    {
+        id:4,
+        name:"something3",
+        number:'4',
+        rating:2.2
+    }
+];
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+      {foodILike.map(dish => (
+              <Food key={dish.id} name={dish.name} number={dish.number} rating={dish.rating}/>
+        ))}
+      </div>
+    );
 }
 
 export default App;
